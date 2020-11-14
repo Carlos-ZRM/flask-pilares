c86517ae0100a316842a2b5cce7cdb3f5f45e9b0
Carlos Reyes <Carlos-ZRM>
2020-11-13 20:02:28 -0600

    michis

--- pilares/app.py
+++ pilares/app.py
@@ -6,6 +6,70 @@ import io
 
 # crear una aplicacion 
 app = Flask(__name__)
+def db_michis ():
+    "JSON es un tipo de documento que define datos"
+   
+    codo = {
+        "nombre":"codo",
+        
+        "ruta": "static\img\codo.jpg"
+        }
+    huevón= {
+       "nombre":"huevón",
+       "ruta":"static\img\huevón.jpg",
+       "peso":5
+        }
+    bizco= {
+       "nombre":"bizco",
+       "ruta":"static\img\ojos.png",
+       "peso":5
+        }
+    jetón ={
+        "nombre":"jetón",
+        "ruta": "static\img\jeton.jpg"
+         }
+    gordo ={
+        "nombre":"gordo",
+        "ruta": "static\img\gordo.jpg"
+         }
+    deamentis ={
+        "nombre":"deamentis",
+        "ruta": "static\img\dibugato.jpg"
+         }
+    tragón ={
+        "nombre":"tragón",
+        "ruta": "static\img\comer.jpg"
+         }
+    cómodo ={
+        "nombre":"cómodo",
+        "ruta": "static\img\wokomodo.jpg"
+         }
+    
+    viajado ={
+        "nombre":"viajado",
+        "ruta": "static\img\psico.jpg"
+         }
+    
+    achis ={
+        "nombre":"achis",
+        "ruta": "static\img\surprise.jpg"
+         }
+    
+    fregón ={
+        "nombre":"fregón",
+        "ruta": "static\img\senor.jpg"
+         }
+    barrio ={
+        "nombre":"barrio",
+        "ruta": "static\img\locos.jpg"
+         }
+    
+    li = {"codo":codo, "huevón":huevón, "bizco":bizco, 
+          "jetón" :jetón, "gordo":gordo, "deamentis":deamentis, 
+          "tragón":tragón, "cómodo": cómodo, "viajado": viajado,
+          "achis":achis, "fregón": fregón, "barrio":barrio
+          }
+    return li
 
 def db_naruto ():
     "JSON es un tipo de documento que define datos"
@@ -101,6 +165,24 @@ def render_bobesponja():
     nombre_d = diccionario_al["nombre"]
     
     return render_template('bobesponja-render.html', imagen_render=ruta, nombre = nombre_d)
+
+@app.route('/michis')
+def render_michis():
+    base_datos = db_michis()
+    llaves = list(base_datos.keys())
+    aleatorio = random.randint(0, len(llaves)-1 ) 
+
+    
+    llave_ale = llaves[aleatorio]
+
+    key = llave_ale
+    diccionario_al = base_datos[key]
+    ruta = diccionario_al["ruta"]
+    nombre_d=diccionario_al["nombre"]
+    
+    
+    
+    return render_template('michis-render.html', imagen_render=ruta, nombre= nombre_d )
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000, host='0.0.0.0')
